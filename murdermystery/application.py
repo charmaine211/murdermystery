dddfea4e02a062785cc88b7f69681e9a5caecc2a
charmaine211 <54951086+charmaine211@users.noreply.github.com>
2020-07-01 18:53:20 +0200

    Import all from helpers

--- murdermystery/application.py
+++ murdermystery/application.py
@@ -7,7 +7,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, special_chars, slogify, deslogify, validate_player, send_invite, checkIfDuplicates, validate_teamhost, teamtable, current_round, max_rounds
+from helpers import *
 
 # Configure application
 app = Flask(__name__)
